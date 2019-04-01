7481c0c995eec8066b24d28450d2c91976588050
manubp <manuelbarroso@outlook.es>
2019-04-01 12:13:21 +0200

    calcularPrecio cración.

--- Tema6ED/src/es/studium/Precio/Precio.java
+++ Tema6ED/src/es/studium/Precio/Precio.java
@@ -2,4 +2,15 @@ package es.studium.Precio;
 
 public class Precio {
 
+	public double calcularPrecio(int unidades, double precioUnitario) {
+		
+		return precioUnitario;
+		
+		
+		
+	
+		
+	}
+	
+	
 }
